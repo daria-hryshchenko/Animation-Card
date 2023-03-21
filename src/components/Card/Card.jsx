23b35e2d54e49513187d7b57961c7de947aec3a2
Daria Hryshchenko <daria199210@gmail.com>
2023-03-22 01:34:28 +0200

    add local storage

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -16,13 +16,22 @@ import {
 export const Card = () => {
   const { id, user, tweets, followers, avatar } = usersData;
 
-  const [isActive, setActive] = useState(false);
-  const [follow, setFollow] = useState(followers);
+  const [isActive, setActive] = useState(
+    JSON.parse(localStorage.getItem('isActive')) || false
+  );
+  const [follow, setFollow] = useState(
+    JSON.parse(localStorage.getItem('follow')) || followers
+  );
 
   const onClick = event => {
     event.preventDefault();
     setActive(!isActive);
-    setFollow(isActive ? follow - 1 : follow + 1);
+    const newValue = !isActive;
+    setActive(newValue);
+    const newFollow = newValue ? follow + 1 : follow - 1;
+    setFollow(newFollow);
+    localStorage.setItem('follow', JSON.stringify(newFollow));
+    localStorage.setItem('isActive', JSON.stringify(newValue));
   };
 
   return (
